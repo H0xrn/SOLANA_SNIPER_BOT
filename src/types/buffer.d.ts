67e01bcd2244d8f73e26b59cf90d0bdc0b8d8629
nelsonmadera <108055094+nelsonmadera@users.noreply.github.com>
2025-05-25 10:16:55 +0700

    Delete src directory

--- src/types/buffer.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'buffer' {
-  export const Buffer: any;
-  export const INSPECT_MAX_BYTES: number;
-  export const kMaxLength: number;
-}
-
-declare module 'buffer/' {
-  export const Buffer: any;
-} 
